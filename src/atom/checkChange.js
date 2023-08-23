4e62ed4af46d49f41d4394a85b5e49d78cc4df5a
taehee.rho <taeheerho@gmail.com>
2023-08-23 21:42:55 +0900

    Feat: 변화를 감지하면 재랜더링

---
+++ src/atom/checkChange.js
@@ -0,0 +1,8 @@
+import { atom } from "recoil";
+
+const checkChange = atom({
+    key: "checkChange",
+    default: false,
+})
+
+export default checkChange;
--- src/components/CreateTodo.jsx
+++ src/components/CreateTodo.jsx
@@ -5,19 +5,28 @@ import Button from "./Button";
 import { useRecoilValue } from "recoil";
 import checkMobile from "../atom/checkMobile";
 import todoAPI from "../api/todoAPI";
+import { useRecoilState } from "recoil";
+import checkChange from "../atom/checkChange";
 
 const CreateTodo = () => {
   const isMobile = useRecoilValue(checkMobile);
   const { createTodo } = todoAPI();
   const [todo, setTodo] = useState({ content: "" });
+  const [isChange, setIsChange] = useRecoilState(checkChange);
+
+  const newTodo = async () => {
+    await createTodo(todo);
+    setIsChange((prev) => !prev);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // createTodo(todoText);
     setTodo((prev) => ({
       ...prev,
       content: "",
     }));
+
+    newTodo();
   };
 
   return (

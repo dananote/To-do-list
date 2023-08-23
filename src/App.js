04a88e9957b0db461948d4b82fd9a0976e9e86ea
taehee.rho <taeheerho@gmail.com>
2023-08-23 16:44:59 +0900

    Chore: recoil 설치 및 mobile 화면 상태  전역으로 관리

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import { Routes, Route } from "react-router-dom";
-import Landing from "./pages/Landing";
 import Todo from "./pages/Todo";
 import GlobalStyle from "./GlobalStyle";
+import Layout from "./Layout";
+import { useMediaQuery } from "react-responsive";
+import { useRecoilState } from "recoil";
+import checkMobile from "./atom/checkMobile";
+import { useEffect } from "react";
 
 function App() {
+  const [isMobile, setIsMobile] = useRecoilState(checkMobile)
+  const Mobile = useMediaQuery({
+    query: "(max-width:800px)"
+  });
+
+  useEffect(() => {
+    setIsMobile(Mobile)
+  }, [Mobile])
+
   return (
     <>
       <GlobalStyle />
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/todo" element={<Todo />} />
-      </Routes>
+      <Layout isMobile={Mobile}>
+        <Routes>
+          <Route path="/" element={<Todo />} />
+        </Routes>
+      </Layout>
     </>
   );
 }
